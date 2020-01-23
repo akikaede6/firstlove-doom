f36bad82ba4fad153a53af52bb50b4c740ec15e9
Chen Li <czxyl@protonmail.com>
2020-01-23 13:20:44 +0800

    I use doom-snippets and which-key

--- packages.el
+++ packages.el
@@ -1,10 +1,10 @@
 ;;; ~/.config/doom/packages.el
 
 ;; `doom-snippets' *is* my personal snippets library (in $DOOMDIR/snippets)
-(package! doom-snippets :disable t)
+;; (package! doom-snippets :disable t)
 
 ;; I never use it.
-(package! which-key :disable t)
+;; (package! which-key :disable t)
 
 ;; For personal modeline
 (package! anzu)
