e6cb7f138064a47b8fe71ae40468e76a41f95174
chenli <czxyl@protonmail.com>
2020-02-01 13:42:54 +0800

    manager lazycat's packages with straight.el

--- packages.el
+++ packages.el
@@ -18,4 +18,29 @@
    :repo "merrickluo/liberime"
    :files ("CMakeLists.txt" "Makefile" "src" "liberime-config.el")))
 
+(package! eaf
+  :recipe
+  (
+   :host github
+   :repo "manateelazycat/emacs-application-framework"))
+
+(package! awesome-tab
+  :recipe
+  (
+   :host github
+   :repo "manateelazycat/awesome-tab"))
+
+(package! snails
+  :recipe
+  (
+   :host github
+   :repo "manateelazycat/snails"))
+
+(package! awesomeshell
+  :recipe
+  (
+   :host github
+   :no-byte-compile t
+   :repo "manateelazycat/aweshell"))
+
 (package! ccls)
