eb21f70e3481453ec42713f0500c4c44bbbe578e
chenli <czxyl@protonmail.com>
2020-02-23 18:26:19 +0800

    add functions for org-journal

--- modules/mylang/org/autoload.el
+++ modules/mylang/org/autoload.el
@@ -12,3 +12,28 @@
   (find-file filename)
   )
 
+
+;;;###autoload
+(defun get-journal-file-today ()
+  "Gets filename for today's journal entry."
+  (let ((daily-name (format-time-string "%Y%m%d")))
+    (expand-file-name (concat org-journal-dir daily-name))))
+
+;;;###autoload
+(defun journal-file-today ()
+  "Creates and load a journal file based on today's date."
+  (interactive)
+  (find-file (get-journal-file-today)))
+
+;;;###autoload
+(defun get-journal-file-yesterday ()
+  "Gets filename for yesterday's journal entry."
+  (let* ((yesterday (time-subtract (current-time) (days-to-time 1)))
+         (daily-name (format-time-string "%Y%m%d" yesterday)))
+    (expand-file-name (concat org-journal-dir daily-name))))
+
+;;;###autoload
+(defun journal-file-yesterday ()
+  "Creates and load a file based on yesterday's date."
+  (interactive)
+  (find-file (get-journal-file-yesterday)))
