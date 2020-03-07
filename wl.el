3944fe9bbdb4a3264fbdc1f875b3824f16b4a10e
Chen Li <chenli@uniontech.com>
2020-03-07 13:52:44 +0800

    fix smtp about wanderlust

--- wl.el
+++ wl.el
@@ -1,10 +1,16 @@
 ;;; ~/.config/doom/wl.el -*- lexical-binding: t; -*-
 
+(setq wl-smtp-connection-type 'nil)
+(setq wl-smtp-posting-port 25)
+(setq wl-smtp-authenticate-type "plain")
+(setq wl-smtp-posting-user "chenli@uniontech.com")
+(setq wl-smtp-posting-server "smtp.263.net")
+(setq wl-local-domain "uniontech.com")
 
 ;; (setq
 ;;  wl-smtp-connection-type 'ssl
 ;;  wl-smtp-posting-port 465
-;;  wl-smtp-authenticate-type "login"
+;;  wl-smtp-authenticate-type "plain"
 ;;  wl-smtp-posting-user "chenli"
 ;;  wl-smtp-posting-server "mail.privateemail.com"
 ;;  wl-local-domain "privateemail.com"
@@ -13,7 +19,6 @@
 ;; 更加复杂的多账号设置
 (setq wl-user-mail-address-list (quote ("chenli@uniontech.com" "czxyl@firstlove.life")))
 
-;;(NOTE: "M-: wl-draft-parent-folder" => %INBOX:myname/clear@imap.gmail.com:993)
 (setq wl-draft-config-alist
       '(((string-match "uniontech.com" wl-draft-parent-folder)
          (template . "uniontech"))
@@ -25,16 +30,15 @@
         (reply "\\(To\\|Cc\\|Delivered-To\\): firstlove.life"
                (template . "firstlove.life"))))
 
-;;choose template with C-c C-j
 (setq wl-template-alist
       '(("uniontech"
+         (wl-smtp-connection-type . 'nil)
          (wl-smtp-posting-port . 25)
-         (wl-from . "陈力 <chenli@uniontech.com>")
          (wl-smtp-authenticate-type . "plain")
-         (wl-smtp-posting-user "chenli@uniontech.com")
-         (wl-smtp-posting-server "smtp.263.net")
-         (wl-local-domain "uniontech.com")
-         (wl-message-id-domain "smtp.263.net")
+         (wl-smtp-posting-user . "chenli@uniontech.com")
+         (wl-smtp-posting-server . "smtp.263.net")
+         (wl-local-domain . "uniontech.com")
+
          )
         ("firstlove.life"
          ("Fcc" . "%INBOX.Sent:\"czxyl@firstlove.life\"/clear@mail.privateemail.com")
@@ -44,5 +48,5 @@
          (wl-smtp-authenticate-type . "plain")
          (wl-smtp-posting-server . "mail.privateemail.com")
          (wl-local-domain . "mail.privateemail.com"))
-         (wl-smtp-posting-port . 465)
+        (wl-smtp-posting-port . 465)
         ))
