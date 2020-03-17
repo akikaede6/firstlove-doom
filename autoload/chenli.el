188578d903b09c76411b502d8c1c0a06e77df7b0
chenli <chenli@uniontech.com>
2020-03-17 14:04:57 +0800

    toggle transparency support

---
+++ autoload/chenli.el
@@ -0,0 +1,7 @@
+;;; ~/.config/doom/autoload/chenli.el -*- lexical-binding: t; -*-
+
+(defun chunyang-toggle-frame-transparency ()
+  (interactive)
+  (if (equal (frame-parameter nil 'alpha) 85)
+      (set-frame-parameter nil 'alpha 100)
+    (set-frame-parameter nil 'alpha 85)))
