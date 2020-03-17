e07811ffa144a1a32019c1c7e601c19070905a03
chenli <chenli@uniontech.com>
2020-03-17 14:05:14 +0800

    add yank bash history support

---
+++ autoload/ivy.el
@@ -0,0 +1,18 @@
+;;; ~/.config/doom/autoload/ivy.el -*- lexical-binding: t; -*-
+
+(defun counsel-yank-bash-history ()
+  "Yank the bash history"
+  (interactive)
+  (let (hist-cmd collection val)
+    (shell-command "history -r") ; reload history
+    (setq collection
+          (nreverse
+           (split-string (with-temp-buffer (insert-file-contents (file-truename "~/.bash_history"))
+                                           (buffer-string))
+                         "\n"
+                         t)))
+    (when (and collection (> (length collection) 0)
+               (setq val (if (= 1 (length collection)) (car collection)
+                           (ivy-read (format "Bash history:") collection))))
+        (kill-new val)
+        (message "%s => kill-ring" val))))
