1ca9221886a859147a201ec348a1b91a619ec6b4
chenli <chenli@uniontech.com>
2020-03-30 13:31:50 +0800

    add more configs for ccls

--- modules/mylang/cc/config.el
+++ modules/mylang/cc/config.el
@@ -9,10 +9,15 @@
   (ccls-use-default-rainbow-sem-highlight)
   (evil-set-initial-state 'ccls-tree-mode 'emacs)
   (setq ccls-initialization-options
-              '(:compilationDatabaseDirectory "build"
-                                              :cache (:directory "/home/firstlove/tmp")
-                                              :index (:threads "16"))
-              ))
-
-
-                                        :clang (:extraArgs ["-I/usr/include/qt" ])
+        '(:compilationDatabaseDirectory "build"
+                                        :clang (:extraArgs ["-I/usr/include/qt" ]
+                                                           :excludeArgs
+                                                           ;; Linux's gcc options. See ccls/wiki
+                                                           ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
+                                                            "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
+                                                            "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
+                                                           )
+                                        :cache (:directory "/home/firstlove/tmp")
+                                        :index (:threads 16)
+                                        )
+        ))
