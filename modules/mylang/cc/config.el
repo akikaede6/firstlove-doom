7dc7b3ef60bb2e1e6121be3fbed18109d4c5be3e
chenli <chenli@uniontech.com>
2020-04-02 18:50:23 +0800

    update ccls

--- modules/mylang/cc/config.el
+++ modules/mylang/cc/config.el
@@ -1,16 +1,20 @@
 ;;; mylang/cc/config.el -*- lexical-binding: t; -*-
 
 (use-package! ccls
-  :hook ((c-mode-local-vars c-mode c++-mode-local-vars objc-mode-local-vars) . (lambda ()(require'ccls)(lsp)(modify-syntax-entry ?_ "w")))
+  :init
+  :hook (((c-mode-local-vars c-mode c++-mode-local-vars objc-mode-local-vars) . (lambda ()(require'ccls)(lsp)(modify-syntax-entry ?_ "w")))
+         (lsp-after-open . ccls-code-lens-mode)
+         (find-file . (lambda ()(require'ccls)(lsp)(modify-syntax-entry ?_ "w")(my-c++-mode-check-buffers)))
+         )
+  ;; :mode ("^D.*" . (lambda ()(require'ccls)(lsp)(modify-syntax-entry ?_ "w")))
   :config
-  (add-hook 'lsp-after-open-hook #'ccls-code-lens-mode)
   (setq ccls-executable "~/projects/github/ccls/build/ccls")
   (setq ccls-sem-highlight-method 'overlay)
   (ccls-use-default-rainbow-sem-highlight)
   (evil-set-initial-state 'ccls-tree-mode 'emacs)
   (setq ccls-initialization-options
         '(:compilationDatabaseDirectory "build"
-                                        :clang (:extraArgs ["-I/usr/include/qt" "-I/usr/include/libdtk-2.1.1/DCore"]
+                                        :clang (:extraArgs ["-I/usr/include/qt" "-I/usr/include/libdtk-2.1.1/DCore" "/usr/include/qt/QtWidgets/"]
                                                            :excludeArgs
                                                            ;; Linux's gcc options. See ccls/wiki
                                                            ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
