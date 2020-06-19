3ae547efbc0fdc0118b772d4998da746b36897ac
chenli <chenli@uniontech.com>
2020-06-19 20:16:16 +0800

    manager multiple mail folders with the help of wl

--- folders.wl
+++ folders.wl
@@ -1,18 +1,177 @@
-%firstlove:"czxyl@firstlove.life"/clear@mail.privateemail.com:993!  "firstlove"
+#
+# Folder definition file
+# This file is generated automatically by Wanderlust/2.15.9 (Almost Unreal).
+#
+# If you edit this file by hand, be sure that comment lines
+# will be washed out by wl-fldmgr.
+#
 
-# Note: usernames with "strange" characters (like "@") must be written
-# like: "myname@company.com". Folders at my work are delimited by dot '.'.
+%firstlove:"czxyl@firstlove.life"/clear@mail.privateemail.com:993!	"firstlove"
 WORK{
-   %INBOX:"chenli@uniontech.com"/clear@imap.263.net:143 "收件箱"
-   %已发送:"chenli@uniontech.com"/clear@imap.263.net:143 "已发送"
-   %已归档:"chenli@uniontech.com"/clear@imap.263.net:143 "已归档"
-   %垃圾邮件:"chenli@uniontech.com"/clear@imap.263.net:143 "垃圾邮件"
-
+    .ToMe
+    .CcMe
+    .colleague
+    .IT
+	linux kernel maillisting{
+          .alpha
+          .announce
+          .api
+          .arch
+          .archive
+          .block
+          .bluetooth
+          .bpf
+          .c
+          .cgroups
+          .devicetree
+          .doc
+          .efi
+          .fbdev
+          .git-commits-head
+          .gpio
+          .janitors
+          .kbuild
+          .kernel
+          .kselftest
+          .linux
+          .linux-arch
+          .man
+          .module
+          .netdev
+          .netdev-owner
+          .next
+          .patching
+          .pci
+          .rcu
+          .rtc
+          .selinux
+          .serial
+          .uring
+          .users
+          .util-linux
+          MH-kernel{
+          owner{
+          +alpha-owner
+          +announce-owner
+          +api-owner
+          +arch-owner
+          +archive-owner
+          +block-owner
+          +bluetooth-owner
+          +bpf-owner
+          +c-owner
+          +cgroups-owner
+          +devicetree-owner
+          +doc-owner
+          +efi-owner
+          +fbdev-owner
+          +gpio-owner
+          +janitors-owner
+          +kbuild-owner
+          +kernel-owner
+          +kselftest-owner
+          +linux-owner
+          +linux-arch-owner
+          +man-owner
+          +module-owner
+          +netdev-owner
+          +netdev-owner-owner
+          +next-owner
+          +patching-owner
+          +pci-owner
+          +rcu-owner
+          +rtc-owner
+          +selinux-owner
+          +serial-owner
+          +uring-owner
+          +users-owner
+          +util-linux-owner
+          +live-patching-owner
+          +linux-serial-owner
+          +linux-security-module-owner
+          +linux-rtc-owner
+          +linux-perf-users-owner
+          +mm-commits
+          +linux-pci-owner
+          +linux-next-owner
+          +linux-man-owner
+          +linux-kselftest-owner
+          +linux-kernel-owner
+          +git-commits-head
+          +linux-next-owner
+          +dwarves-owner
+          +io-uring-owner
+          +linux-gpio-owner
+          +linux-api-owner
+          +linux-bluetooth-owner
+          +linux-kbuild-owner
+          +linux-perf-users-owner
+          +linux-perf-users-owner
+          +linux-alpha-owner
+          +linux-efi-owner
+          +linux-block-owner
+          +linux-doc-owner
+          }
+          +alpha
+          +announce
+          +api
+          +arch
+          +archive
+          +block
+          +bluetooth
+          +bpf
+          +c
+          +cgroups
+          +devicetree
+          +doc
+          +efi
+          +fbdev
+          +gpio
+          +janitors
+          +kbuild
+          +kernel
+          +kselftest
+          +linux
+          +linux-arch
+          +man
+          +module
+          +netdev
+          +next
+          +patching
+          +pci
+          +rcu
+          +rtc
+          +selinux
+          +serial
+          +uring
+          +users
+          +util-linux
+          }
+	}
+    .PMS
+    .openssh
+	%ltp-try:"chenli@uniontech.com"/clear@imap.263.net:143	"ltp-try-show"
+	%已归档:"chenli@uniontech.com"/clear@imap.263.net:143	"已归档"
+	%已发送:"chenli@uniontech.com"/clear@imap.263.net:143	"已发送"
+	%INBOX:"chenli@uniontech.com"/clear@imap.263.net:143	"收件箱"
+	%垃圾邮件:"chenli@uniontech.com"/clear@imap.263.net:143	"垃圾邮件"
+    .archive
+    MH{
+    +PMS
+    +openssh
+    +Majordomo
+    +draft
+    +ltp
+    }
+    .deepin
 }
-
 NEWS {
-    @rss-hash.xkcd "xkcd"
-    @rss-hash.acgpiping "ACG"
-    @rss-hash.moonlight "MoonLight"
-    @rss-hash.interrupt-memfault "interrupt-memfault"
+	@rss-hash.xkcd	"xkcd"
+	@rss-hash.acgpiping	"ACG"
+	@rss-hash.moonlight	"MoonLight"
+	@rss-hash.interrupt-memfault	"interrupt-memfault"
 }
+
+# petname definition (access group, folder in access group)
+
+# end of file.
--- wl.el
+++ wl.el
@@ -38,7 +38,17 @@
          (wl-smtp-posting-user . "chenli@uniontech.com")
          (wl-smtp-posting-server . "smtp.263.net")
          (wl-local-domain . "uniontech.com")
-
+         (wl-refile-rule-alist .       '(("x-ml-name"
+                                          ("^Wanderlust" . "+wl")
+                                          ("^Elisp" . "+elisp"))
+                                         ;; (("To" "Cc")
+                                         ;;  ("\\([a-z]+\\)@gohome\\.org" . "+\\1"))
+                                         ;; ("From"
+                                         ;;  ("me@gohome\\.org" . ("To" ("you@gohome\\.org" .
+                                         ;;                              "+from-me-to-you"))))
+                                         (("Subject" "To")
+                                          ("\\([a-zA-Z-]+\\)@vger\\.kernel\\.org" . "+\\1"))
+                                         ))
          )
         ("firstlove.life"
          ;; ("Fcc" . "%INBOX.Sent:\"czxyl@firstlove.life\"/clear@mail.privateemail.com")
@@ -50,3 +60,104 @@
          (wl-local-domain . "mail.privateemail.com"))
         (wl-smtp-posting-port . 465)
         ))
+
+(setq wl-subscribed-mailing-list
+      '("wl@ml.gentei.org"
+        "apel-ja@m17n.org"
+        "linux-alpha@vger.kernel.org"
+        "linux-media@vger.kernel.org"
+        "linux-kernel@vger.kernel.org"
+        "linux-rtc@vger.kernel.org"
+        "backports@vger.kernel.org"
+        "linux-x11@vger.kernel.org"
+        "linux-apps@vger.kernel.org"
+        "linux-assembly@vger.kernel.org"
+        "io-uring@vger.kernel.org"
+        "linux-arch@vger.kernel.org"
+        "kernel-testers@vger.kernel.org"
+        "git-commits-head@vger.kernel.org"
+        "linux-api@vger.kernel.org"
+        "ceph-devel@vger.kernel.org"
+        "linux-admin@vger.kernel.org"
+        "kernel-packagers@vger.kernel.org"
+        "dwarves@vger.kernel.org"
+        "linux-gcc@vger.kernel.org"
+        "linux-console@vger.kernel.org"
+        "linux-c-programming@vger.kernel.org"
+        "linux-gpio@vger.kernel.org"
+        "linux-bluetooth@vger.kernel.org"
+        "linux-efi@vger.kernel.org"
+        "linux-doc@vger.kernel.org"
+        "linux-kbuild@vger.kernel.org"
+        "bpf@vger.kernel.org"
+        "linux-btrace@vger.kernel.org"
+        "linux-block@vger.kernel.org"
+        "linux-fbdev@vger.kernel.org"
+        "cgroups@vger.kernel.org"
+        "linux-config@vger.kernel.org"
+        "devicetree-spec@vger.kernel.org"
+        "kernel-janitors@vger.kernel.org"
+        "linux-hotplug@vger.kernel.org"
+        "linux-kernel-announce@vger.kernel.org"
+        "linux-kselftest@vger.kernel.org"
+        "linux-kernel@vger.kernel.org"
+        "linux-i2c@vger.kernel.org"
+        "linux-laptop@vger.kernel.org"
+        "linux-modules@vger.kernel.org"
+        "linux-man@vger.kernel.org"
+        "devicetree@vger.kernel.org"
+        "linux-new-lists@vger.kernel.org"
+        "linux-newbie@vger.kernel.org"
+        "linux-next@vger.kernel.org"
+        "linux-numa@vger.kernel.org"
+        "linux-perf-users@vger.kernel.org"
+        "linux-security-module@vger.kernel.org"
+        "linux-pci@vger.kernel.org"
+        "linux-serial@vger.kernel.org"
+        "linux-sound@vger.kernel.org"
+        "linux-standards@vger.kernel.org"
+        "linux-trace-devel@vger.kernel.org"
+        "linux-trace-users@vger.kernel.org"
+        "live-patching@vger.kernel.org"
+        "netdev@vger.kernel.org"
+        "linux-smp@vger.kernel.org"
+        "rcu@vger.kernel.org"
+        "perfbook@vger.kernel.org"
+        "selinux@vger.kernel.org"
+        "trinity@vger.kernel.org"
+        "util-linux@vger.kernel.org"
+        "workflows@vger.kernel.org"
+        "ltp@lists.linux.it"
+        "emacs-mime-ja@m17n.org"))
+(setq wl-summary-indent-length-limit 300)
+(setq wl-summary-width 300)
+
+(setq wl-refile-rule-alist
+      '(("x-ml-name"
+         ("^Wanderlust" . ".wl")
+         ("^Elisp" . ".elisp"))
+        ;; (("To" "Cc")
+        ;;  ("\\([a-z]+\\)@gohome\\.org" . "+\\1"))
+        ;; ("From"
+        ;;  ("me@gohome\\.org" . ("To" ("you@gohome\\.org" .
+        ;;                              "+from-me-to-you"))))
+        (("To")
+         ("chenli@uniontech\\.com" . ".ToMe")
+         ("git-commits-head@vger\\.kernel\\.org" . ".git-commits-head"))
+        (("Cc")
+         ("chenli@uniontech\\.com" . ".CcMe"))
+        (("Subject" "To")
+         ("\\([a-zA-Z-]+\\)-owner@vger\\.kernel\\.org" . ".\\1")
+         ("ltp@lists\\.linux\\.it" . "%ltp-try:\"chenli@uniontech.com\"/clear@imap.263.net:143"  )
+         ("openssh-unix-dev" . ".openssh")
+         )
+        ( ("From")
+          ("it@uniontech.com" . ".IT")
+         ("PMS" . "+PMS")
+         ("Majordomo@vger\\.kernel\\.org" . ".archive")
+         (".*@uniontech.com" . ".colleague")
+         ("*@deepin.com" . ".deepin")
+         )
+        ))
+
+(setq wl-summary-auto-refile-skip-marks nil)
